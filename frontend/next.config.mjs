1d587a8a8e89568159c84873a9a143bea93d487f
Dimas Cahyo Margono <dimascahyomg855@gmail.com>
2026-05-05 08:55:41 +0700

    hosting: setup firebase hosting for frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.figma.com",
-        pathname: "/api/mcp/asset/**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
